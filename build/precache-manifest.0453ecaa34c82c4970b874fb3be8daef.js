42f5576f76b57892e9faa795492d47c5226ef81e
ajaykirtikar <ajaykirtikar2@gmail.com>
2020-08-15 17:02:00 -0500

    show build folder

---
+++ build/precache-manifest.0453ecaa34c82c4970b874fb3be8daef.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af68d0111f08c28f6f46175a874e8386",
+    "url": "/index.html"
+  },
+  {
+    "revision": "341bb0257921cd33e689",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d8a53b72dbd416f2de12",
+    "url": "/static/css/main.6bda8b97.chunk.css"
+  },
+  {
+    "revision": "341bb0257921cd33e689",
+    "url": "/static/js/2.ce613d3e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ce613d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a53b72dbd416f2de12",
+    "url": "/static/js/main.d0ccea24.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "812958929d7e992165227c5bf429cd45",
+    "url": "/static/media/Resume2020.81295892.pdf"
+  },
+  {
+    "revision": "43b0150f0c469c8d52f6f6998c1d2325",
+    "url": "/static/media/gears.43b0150f.png"
+  },
+  {
+    "revision": "3b6c2229c6b2c3637aaa8c1041adc4fa",
+    "url": "/static/media/pathway.3b6c2229.png"
+  },
+  {
+    "revision": "93ed410de6b2b9603883729e237661e0",
+    "url": "/static/media/rap-robot.93ed410d.png"
+  }
+]);
